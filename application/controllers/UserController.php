d3b8e55a07dc35debd84fbbfcaf4f55da3be3b85
junirii <ga243@naver.com>
2022-07-15 18:09:15 +0900

    first commit

---
+++ application/controllers/UserController.php
@@ -0,0 +1,17 @@
+<?php
+namespace application\controllers;
+
+class UserController extends Controller {
+  public function signup(){
+    $json = getJson();
+    $result = $this->model->signUp($json);
+    if($result){
+      $loginUser = [
+        
+      ];
+      $this->flash(_LOGINUSER, $result);
+      return [_RESULT => 1];
+    }
+    return [_RESULT => 0];
+  }
+}
